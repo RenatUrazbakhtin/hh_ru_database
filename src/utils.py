8fe5c8efa2bb2abf6c7fd2ddd7992de4b02c37d6
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-08-11 02:22:23 +0300

    funcs for getting info from hh

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+###яндекс, ВК, деливери, Тинькоф, Сбер сервис, ВТБ, инфотекс, мтс, КРОК, АЙ-ТЕКО
+import requests, json
+def get_hh_data_employers():
+    companies_id = [1740, 15478, 592442, 78638, 1473866, 4181, 3778, 3776, 2987, 115]
+    list = []
+    for id in companies_id:
+        params = {
+            'employer_id': id,
+            'per_page': 30
+        }
+        request = requests.get("https://api.hh.ru/vacancies", params=params)
+        data = request.json()['items']
+        list.append(data)
+    return list
+
+def get_necessary_vacancy_info():
+    list = []
+    for item in get_hh_data_employers():
+        for vacancy in item:
+            dict = {"vacancy_name": vacancy['name'], "salary": vacancy['salary'], "employee": vacancy['employer']['name'], "vacancy_url": vacancy['alternate_url'], "employment": vacancy['employment']['name'], "experience": vacancy['experience']['name'], "requirements": vacancy['snippet']['requirement']}
+            list.append(dict)
+    return list
+
