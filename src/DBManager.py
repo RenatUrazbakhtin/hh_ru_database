ac55e0359c948638549dac62c9c12e31e6d604cb
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-08-13 17:55:12 +0300

    init, get_companies func

--- src/DBManager.py
+++ src/DBManager.py
@@ -1,17 +1,18 @@
 import psycopg2
-
+import config
 class DBmanager():
+    def __init__(self, database_name, params):
+        self.conn = psycopg2.connect(dbname=database_name, **params)
+        self.cur = self.conn.cursor()
+
     def get_companies_and_vacancies_count(self):
-        pass
+        with self.conn:
+            self.cur.execute("""select employer_id, title, count(vacancies.vacancy_id) as vacancies_number from employers join vacancies using(employer_id) group by employer_id""")
+            data = self.cur.fetchall()
+            for item in data:
+                dict = [{"employer_id": item[0], "title": item[1], "vacancies_number": item[2]}]
+        return dict
 
-    def get_all_vacancies(self):
-        pass
 
-    def get_avg_salary(self):
-        pass
 
-    def get_vacancies_with_higher_salary(self):
-        pass
 
-    def get_vacancies_with_keyword(self):
-        pass
