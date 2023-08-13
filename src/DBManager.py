75aa7cf909713902bba0e89d4d719499c3fb796f
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-08-13 17:56:27 +0300

    get_all_vacancies commit

--- src/DBManager.py
+++ src/DBManager.py
@@ -13,6 +13,14 @@ class DBmanager():
                 dict = [{"employer_id": item[0], "title": item[1], "vacancies_number": item[2]}]
         return dict
 
-
+    def get_all_vacancies(self):
+        with self.conn:
+            self.cur.execute(
+                "select vacancy_id, employer, vacancy_name, salary_from, salary_to, vacancy_url from vacancies join employers using(employer_id)")
+            data = self.cur.fetchall()
+            for item in data:
+                dict = [{"vacancy_id": item[0], "employer": item[1], "vacancy_name": item[2], "salary_from": item[3],
+                         "salary_to": item[4], "vacancy_url": item[5]}]
+        return dict
 
 
