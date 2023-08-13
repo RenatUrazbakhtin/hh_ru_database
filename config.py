8aeced4b49b83a30ea379ad8efe5e201246bc493
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-08-13 18:23:27 +0300

    changes for correct work

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from configparser import ConfigParser
 
-def config(filename="database.ini", section="postgresql"):
+
+def config(filename="src\database.ini", section="postgresql"):
     # create a parser
     parser = ConfigParser()
     # read config file
@@ -14,3 +15,5 @@ def config(filename="database.ini", section="postgresql"):
         raise Exception(
             'Section {0} is not found in the {1} file.'.format(section, filename))
     return db
+
+
